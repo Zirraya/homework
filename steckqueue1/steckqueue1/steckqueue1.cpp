a4c7c4d55d9a98c22fd6f01e6c94304a15c03d40
Harpie <silvia.lesnaia@gmail.com>
2024-06-02 14:24:10 +0400

    Задание по инфопроге за 2 семестр. Это стеки и очереди. Задание 2 и 3 работают немного некорректно

---
+++ steckqueue1/steckqueue1/steckqueue1.cpp
@@ -0,0 +1,83 @@
+﻿// steckqueue1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+bool isComposite(int num) {
+    if (num <= 1) return false; // 0 and 1 are not composite
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return true; // found a divisor, so it's composite
+    }
+    return false; // no divisors found, so it's prime
+}
+
+   queue<int> shiftQueue(std::queue<int> q) {
+    vector<int> vec;
+    while (!q.empty()) {
+        vec.push_back(q.front());
+        q.pop();
+    }
+
+    int compositeIndex = -1;
+    for (int i = 0; i < vec.size(); ++i) {
+        if (isComposite(vec[i])) {
+            compositeIndex = i;
+            break;
+        }
+    }
+
+    if (compositeIndex != -1) {
+        rotate(vec.begin(), vec.begin() + compositeIndex, vec.end());
+    }
+
+    queue<int> result;
+    for (int num : vec) {
+        result.push(num);
+    }
+
+    return result;
+}
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+    queue<int> q;
+
+    int num;
+    cout << "Введите очередь (введите 0 что бы закончить введение): ";
+    while (true) {
+        cin >> num;
+        if (num == 0) break;
+        q.push(num);
+    }
+      
+    cout << endl; cout << "Очередь псоле циклического свдига: ";
+
+    q = shiftQueue(q);
+
+    while (!q.empty()) {
+           cout << q.front() << " ";
+        q.pop();
+    }
+
+    return 0;
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ stekqueue2/stekqueue2/stekqueue2.cpp
@@ -0,0 +1,79 @@
+﻿// stekqueue2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+// Функция для вставки четного элемента
+void insertLastEven(queue<int>& q, int lastEven, int minNum) {
+	queue<int> temp;
+	while (!q.empty()) {
+		if (q.front() == minNum) {
+			temp.push(lastEven);
+		}
+		temp.push(q.front());
+		q.pop();
+	}
+	q = temp;
+}
+
+// Функция для нахождения минимального элемента
+int findMin(queue<int> qCopy) { // Создание копии
+	int minNum = qCopy.front();
+	while (!qCopy.empty()) {
+		if (qCopy.front() < minNum) minNum = qCopy.front();
+		qCopy.pop();
+	}
+	return minNum;
+}
+
+int main()
+{
+
+	setlocale(LC_ALL, "RUS");
+
+	queue<int> q;
+
+	int num;
+	cout << "Введите очередь (введите 0 что бы закончить введение): ";
+	while (true) {
+		cin >> num;
+		if (num == 0) break;
+		q.push(num);
+	}
+
+	queue<int> qCopy = q; // Создание копии
+	int minNum = findMin(qCopy); // Нахождение минимального элемента
+
+	qCopy = q; // Сбросить копию
+	int lastEven = -1;
+	while (!qCopy.empty()) {
+		if (qCopy.front() % 2 == 0) lastEven = qCopy.front();
+		qCopy.pop();
+	}
+
+	insertLastEven(q, lastEven, minNum);
+
+	cout << "Измененная очередь: ";
+	while (!q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+	cout << endl;
+
+	return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
