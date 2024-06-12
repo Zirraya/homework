ffea52254175d5d0b27b5f9babb115bba630d11e
Harpie <silvia.lesnaia@gmail.com>
2024-06-12 22:36:15 +0400

    Домашка по инфопроге за 1 семестр. Я вообще не знаю что это. Но пусть будет

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,113 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+double Series(double x, double eps)
+{
+    double term = x * x * x / 6, sum = term;
+    x = x * x;
+    for (int n = 4; abs(term) > eps; n += 2)
+        sum += term *= -x / (n * (n + 1));
+    return sum;
+}
+
+
+int main()
+    
+{
+    
+    for (double x = 0; x < 1; x += 0.1)
+        cout << setw(5) << x << setw(12) << x - sin(x) << setw(12) << Series(x, 1e-7) << endl;
+}
+
+
+
+
+//int i, n, fl;
+//double z;
+//cout << "n=";
+//cin >> n;
+//system("cls");
+//
+//double* a = new double[n];
+//
+//for (i = 0; i < n; i++)
+//{
+//    cout << "a[" << i + 1 << "]=";
+//    cin >> a[i];
+//    cout << "\n";
+//}
+//system("cls");
+//do
+//{
+//    fl = 0;
+//    for (i = 0; i < n - 1; i++)
+//    {
+//        if (a[i] > a[i + 1]) { fl = 1; z = a[i]; a[i] = a[i + 1]; a[i + 1] = z; }
+//    }
+//} while (fl == 1);
+//
+//
+//z = a[0];
+//a[0] = a[n - 1];
+//a[n - 1] = z;
+//
+//for (i = 0; i < n; i++)
+//{
+//    cout << a[i] << " ";
+//}
+//
+//cout << "\n\n\n";
+//system("pause");
+//delete[] a;
+  
+
+
+//setlocale(LC_ALL, "RUS");
+//int a, b, h;
+//cout << "Введите игнтервал (a < b):\n";
+//cout << "a = ";
+//cin >> a;
+//cout << "b = ";
+//cin >> b;
+//cout << "\nEnter a step:\n";
+//cout << "h = ";
+//cin >> h;
+//cout << "\nOutput of the program:\n";
+//do {
+//    cout << a << " ";
+//    a += h;
+//} while (a <= b);
+//system("pause");
+//return 0;
+
+
+
+ /*   setlocale(LC_ALL, "RUS");
+    int a, b, c, X;
+    cout << "Enter integer a, b, c, X:\n";
+    cin >> a >> b >> c >> X;
+
+    if ((a + b + c) % 2 == 0 && (a + b + c) % X == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    system("pause");
+    return 0;
+}*/
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ struct1/struct1/struct1.cpp
@@ -0,0 +1,89 @@
+﻿// struct1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+using namespace std;
+
+struct Train {
+    string namePynkt;
+    int numTrain;
+    string timeArrive;
+};
+
+Train putTrain(Train t) {
+    cout << "\n Введите номер пунтка - ";
+    cin >> t.namePynkt;
+    cout << "\n Введите номер позеда - ";
+    cin >> t.numTrain;
+    cout << "\n Введите время прибытиые - ";
+    cin >> t.timeArrive;
+
+    return t;
+}
+
+void outTrain(Train t) {
+    cout << "\nНомер: " << t.numTrain << " Путь: " << t.namePynkt << " Прибытие: " << t.timeArrive;
+}
+
+void outTrainList(Train t[], int  size) {
+    for (int i = 0; i < size; i++) {
+        outTrain(t[i]);
+    }
+}
+
+void swap(Train& x, Train& y)
+{
+    Train temp = x;
+    x = y;
+    y = temp;
+}
+
+void showInfoSort(Train t[], int  size) {
+    cout << "\n__________" << "Таблица времени поезда" << "__________";
+    for (int i = 1; i < size; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            if (t[i].numTrain < t[j].numTrain) swap(t[i], t[j]);
+        }
+    }
+    outTrainList(t, size);
+
+}
+
+int main()
+{
+    int size = 5, numTrainEnter;
+    bool found = false;
+    Train TrainList[size];
+    for (int i = 0; i < size; i++) {
+        TrainList[i] = putTrain(TrainList[i]);
+    }
+    cout << "Введите номер поезда:";
+    cin >> numTrainEnter;
+    for (int i = 0; i < size; i++) {
+        if (numTrainEnter == TrainList[i].numTrain) {
+            outTrain(TrainList[i]);
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "\n Информация о поезде найдена";
+    }
+
+    showInfoSort(TrainList, size);
+
+    system("pause");
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
