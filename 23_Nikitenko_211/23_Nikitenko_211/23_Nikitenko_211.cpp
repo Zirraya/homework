03d433c2d73cdd5558b341f4de782f30d2b475f1
Harpie <silvia.lesnaia@gmail.com>
2024-09-24 22:05:48 +0400

    Задание по ООП. Почти готово. Фукнции реализованы и адекватно работают со списком. Осталось внести последние штрихи - сделать сравнение, перегрузку и прочие

--- 23_Nikitenko_211/23_Nikitenko_211/23_Nikitenko_211.cpp
+++ 23_Nikitenko_211/23_Nikitenko_211/23_Nikitenko_211.cpp
@@ -3,6 +3,8 @@
 
 // Я решила выбрать саламндру, так как, они милые UwU
 
+// 1 лаба 4 задания 2 лаба 6 задания
+
 #include <iostream>
 
 using namespace std;
@@ -53,67 +55,6 @@ public:
 	string getPattern() { return Pattern; }
 	string getSpesies() { return Species; }
 
-
-
-
-
-	// Функия для выбора саламандры
-	void ChoiceSalamander() {
-		int choice;
-
-		cout << "Вы выбираете салмандру..." << endl;
-		cout << "Вариант 1: Тигровая" << endl;
-		cout << "Вариант 2: Пятнистая" << endl;
-		cout << "Вариант 3: Красноспинная" << endl;
-		cout << "Вариант 4: Северная" << endl;
-		cout << "Вариант 5: Северозападная" << endl;
-
-		cout << "Вариант 6: Вы решаете внимательно пригледется к земноводным..." << endl;
-		cout << "Вариант 7: Вы подумали неплохо бы найти человека которорый знает об этих созданиях хоть что-то..." << endl;
-
-		cout << "Какой выриант вы выбиараете? ";
-		cin >> choice;
-
-		switch (choice) {
-		case 1:
-			Species = "Тигровая";
-			break;
-		case 2:
-			Species = "Пятнистая";
-			break;
-		case 3:
-			Species = "Красноспинная";
-			break;
-		case 4:
-			Species = "Северная";
-			break;
-		case 5:
-			Species = "Северозападная";
-			break;
-		case 6:
-			InfoSalamander();
-		default:
-			cout << "Неверный выбор!" << endl;
-			return; // Завершение если выбран неверный вариант
-		}
-	}
-
-	//
-
-	// Вывод инцы про саламандар по первому взгляду
-	void InfoSalamander() {
-		cout << "Рязглядывая этих созданий вы приходите к следующему выводу\n";
-		cout << "Тигровая явно не настроена что бы ее трогали. Возможно контакт с ней закончится плачевно\n";
-		cout << "Пятнистая смотрит на вас настороженно. Лучше оставить ее в покое\n";
-		cout << "Красноспинная активно размахивает хвостом глядя на вас. Может к ней не стоит сувать свои руки?\n";
-		cout << "Северная глядит на вас спокойны взлядом. Она выглядит вполне дружелюбно\n";
-		cout << "Северозападная с интересом разглядывает вас. В прицнипе она не выглядит агресивно\n";
-
-		ChoiceSalamander();
-
-	}
-
-
 	// сравнение саламандер на сходство по признакам - взяли двух саламандер и 
 	// задали им условие сравнения - условие будет любым
 	bool  similarity(BlueSpottedSalamander& sp, bool condition(BlueSpottedSalamander& sp1, BlueSpottedSalamander& sp2)) {
@@ -130,37 +71,76 @@ public:
 	// Точная ифнормация о саламандрах
 		friend ostream& operator<< (ostream & out, BlueSpottedSalamander sp) 
 		{
-			out << "Саламндра по имени" << sp.Name << " Цвета" << sp.ColorMane << "Ее цвет узора" << sp.ColorPattern << "Длина" << sp.Lengh << "Возраста" << sp.Age
-				<< "Ее любимая еда" << sp.FavoriteFood << "Ее узор" << sp.Pattern << "\n";
+			out << "Саламндра по имени" << sp.Name << " Цвета " << sp.ColorMane << " Ее цвет узора " << sp.ColorPattern << " Длина " << sp.Lengh << " Возраста " << sp.Age
+				<< " Ее любимая еда " << sp.FavoriteFood << " Ее узор " << sp.Pattern << "\n";
 			return out;
 		}
 	
 	//
 
+
+
+
+
+
+	// Функция для взгляда на саламандр
+		bool ToLook() {
+
+			cout << "Рязглядывая этих созданий вы приходите к следующему выводу\n " << endl;
+
+			if (Species == " Тигровая ") {
+				cout << "Тигровая явно не настроена что бы ее трогали.Возможно контакт с ней закончится плачевно\n" << endl;
+				return false;
+			}
+			else if (Species == " Пятнистая ") {
+				cout  << "Пятнистая смотрит на вас настороженно. Лучше оставить ее в покое\n" << endl;
+				return false;
+			}
+			else if (Species == " Красноспинная ") {
+				cout << "Красноспинная активно размахивает хвостом глядя на вас. Может к ней не стоит сувать свои руки?" << endl;
+				return false;
+			}
+			else if (Species == " Северная ") {
+				cout << "Северная глядит на вас спокойны взлядом. Она выглядит вполне дружелюбно\n" << endl;
+				return true;
+			}
+			else if (Species == " Северозападная ") {
+				cout << "Северозападная с интересом разглядывает вас. В прицнипе она не выглядит агресивно\n" << endl;
+				return true;
+			}
+			return false;
+
+		}
+
 	// Реакция
-	void ToReact() {
+	bool ToReact() {
 
 		cout << "Вы решаете погладить " << Name << endl;
 
-		if (Species == "Тигровая") {
+		if (Species == " Тигровая ") {
 			cout << Name << " Наносит мощный удар хвостом!" << endl;
 			actionPet();
+			return false;
 		}
-		else if (Species == "Пятнистая") {
+		else if (Species == " Пятнистая ") {
 			cout << Name << " Делает быстрый удар хвостом!" << endl;
 			actionPet();
+			return false;
 		}
-		else if (Species == "Красноспинная") {
+		else if (Species == " Красноспинная ") {
 			cout << Name << " Яростно машет хвостом!" << endl;
 			actionPet();
+			return false;
 		}
-		else if (Species == "Северная") {
+		else if (Species == " Северная ") {
 			cout << Name << " Кажется ей нравится!" << endl;
+			return true;
 		}
-		else if (Species == "Северозападная") {
+		else if (Species == " Северозападная ") {
 			cout << Name << " В замашатльстве!" << endl;
-
+			return true;
 		}
+		return false;
 
 	}
 	//
@@ -183,8 +163,10 @@ public:
 		List() { head = NULL; tail = NULL; }
 		void push(BlueSpottedSalamander* sp);
 		void print();
+		void ListPrint();
 		bool find(BlueSpottedSalamander sp); // Найти  одну нужную саламандру
 		bool findAll(bool condition(BlueSpottedSalamander& sp1)); // Найти всех соломандр по условию
+		BlueSpottedSalamander& getByID(int id);
 
 	};
 
@@ -214,6 +196,19 @@ public:
 		cout << '\n';
 	}
 
+	void List::ListPrint()
+	{
+		node* r = head;
+		int i = 1;
+
+		while (r != NULL) {
+			cout << "Вариант " << i  << ": " << r->inf->getName() << endl;
+			r = r->next;
+			i++;
+		}
+		cout << '\n';
+	}
+
 	bool List::find(BlueSpottedSalamander sp) {
 		node* r = head;
 		while (r != NULL) {
@@ -237,7 +232,29 @@ public:
 		return (r != NULL);
 	}
 
-//
+
+	bool CondFavorFoodAndName(BlueSpottedSalamander& sp) {
+		return (sp.getFavoriteFood() == " Бабочки " && sp.getName() == " Сумрак");
+	}
+
+	bool CondColorPatternAndSpesies(BlueSpottedSalamander& sp) {
+		return (sp.getColorPattern() == " Синий " && sp.getSpesies() == "Тигровая");
+	}
+
+	BlueSpottedSalamander& List::getByID(int id) {
+		node* r = head;
+		int i = 1;
+
+		while (r != NULL) {
+			if (i == id)
+			{
+				return *(r->inf);
+			}
+			i++;
+			r = r->next;
+		}
+	}
+
 
 
 
@@ -245,28 +262,66 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 
-	cout << "Несколько саламандр греются на солнышке\n";
-	cout << "Вы решаете погладить их...\n";
-
 	//string N, string CM, string CP, float L, int A, string FF, string P, string S
-	BlueSpottedSalamander sp1( " Огниво ", " Черный ", " Оранжевый ", 21.0, 5, " Моллюски ", " Полосатая ", " Тигровая "); cout << sp1;
-	BlueSpottedSalamander sp2(" Винтер ", " Черный ", " Синий ", 8.0, 17, " Черви ", " Полосатая ", " Пятнистая "); cout << sp2;
-	BlueSpottedSalamander sp3(" Рэдди ", " Красный ", " Черный ", 9.0, 8, " Пауки ", " Полосатая ", " Красноспинная "); cout << sp3;
-	BlueSpottedSalamander sp4(" Сумрак ", " Бежевый ", " Тмено-коричневый ", 11.0, 7, " Бабочки  ", " Пятнистая ", " Северная "); cout << sp4;
-	BlueSpottedSalamander sp5(" Можжевельник ", " Коричневый ", " Оранжевый ", 9.0, 27, " Гусиницы ", " Крапинки ", " Северозападная "); cout << sp5;
+	BlueSpottedSalamander sp1(" Огниво", " Черный ", " Оранжевый ", 21.0, 5, " Моллюски ", " Полосатая ", " Тигровая "); cout << sp1;
+	BlueSpottedSalamander sp2(" Винтер", " Черный ", " Синий ", 8.0, 17, " Черви ", " Полосатая ", " Пятнистая "); cout << sp2;
+	BlueSpottedSalamander sp3(" Рэдди", " Красный ", " Черный ", 9.0, 8, " Пауки ", " Полосатая ", " Красноспинная "); cout << sp3;
+	BlueSpottedSalamander sp4(" Сумрак", " Бежевый ", " Тмено-коричневый ", 11.0, 7, " Бабочки ", " Пятнистая ", " Северная "); cout << sp4;
+	BlueSpottedSalamander sp5(" Можжевельник", " Коричневый ", " Оранжевый ", 9.0, 27, " Гусиницы ", " Крапинки ", " Северозападная "); cout << sp5;
+
+	cout << endl;
+
+	BlueSpottedSalamander sp;
 
 	List lstSalamander;
-	if (lstSalamander.find(sp1))
-		lstSalamander.push(&sp1);
-	else if (sp1.ToReact(" Наносит мощный удар хвостом! "))
-		lstSalamander.push(&sp1);
+
+	lstSalamander.push(&sp1);
+	lstSalamander.push(&sp2);
+	lstSalamander.push(&sp3);
+	lstSalamander.push(&sp4);
+	lstSalamander.push(&sp5);
 
 
 
+	// Функия для выбора саламандры
+		int choice;
+		int choiseMove;
 
-	BlueSpottedSalamander salamander;
-	salamander.ChoiceSalamander(); // Выбор саламандры
-	salamander.ToReact(); // Реакция саламандры
+		cout << "Несколько саламандр греются на солнышке\n";
+		cout << "Вы выбираете салмандру..." << endl;
+
+		lstSalamander.ListPrint();
+
+		cout << "C какой саламандрой вы хоите познакомиться поближе (Введите номер варианта)? ";
+		cin >> choice;
+		BlueSpottedSalamander b = lstSalamander.getByID(choice);
+
+
+		cout << "Вариант 1: Вы решаете погладить"<< endl; cout << "Вариант 2: Вы решаете присмотреться"<< endl;
+
+
+		cout << "Что вы хотите сделать? ";
+		cin >> choiseMove;
+
+		switch (choiseMove)
+		{
+		case 1:
+			b.ToReact();
+			break;
+		case 2:
+			b.ToLook();
+		default:
+			cout << "Вы уходите" << endl;
+			
+		}
+
+
+		
+		cout << "\n Ваш друг решил приютить пару саламандр. он выбрал саламандру с синим узором и тигровую \n";
+		lstSalamander.findAll(CondColorPatternAndSpesies);
+		
+		cout << "\n Вам захотели к себе тоже взять саламандр. Вы выбрали ту которая любит бабочек -  дружелюбную Сумрак\n";
+		lstSalamander.findAll(CondFavorFoodAndName);
 
 	system("pause");
 
