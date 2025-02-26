f461b5ecccf2ce2f4c57b38ab2c1725afd7f89a6
Harpie <silvia.lesnaia@gmail.com>
2025-02-26 18:55:52 +0400

    Сортировка подсчетом обновление. 1 задание

--- ADA/CountingortNikitenko/CountingortNikitenko/CountingortNikitenko.cpp
+++ ADA/CountingortNikitenko/CountingortNikitenko/CountingortNikitenko.cpp
@@ -8,26 +8,38 @@
 using namespace std;
 
 // Функция сортировки
-void countingSort(vector<int>& arr) {
-    if (arr.empty()) return;
+vector<int> countSort(vector<int>& inputArray)
+{
+    int N = inputArray.size();
+    int M = 0; // Нахождение максимального элемента массива inputArray[]. 
 
-    int maxVal = *max_element(arr.begin(), arr.end()); // Максимальное элемент массива
-    vector<int> count(maxVal + 1, 0); // Вектор для подсчета частоты
+    for (int i = 0; i < N; i++)
+        M = max(M, inputArray[i]);
 
-    // Подсчет вхождений каждого элемента
-    for (int num : arr) {
-        count[num]++;
-      
-        // Перезапись оригинального массива
-        int index = 0;
-        for (int i = 0; i <= maxVal; i++) {
-            while (count[i] > 0) {
-                arr[index++] = i;
-                count[i]--;
-            }
-        }
-        //
+   
+    vector<int> countArray(M + 1, 0); // Массив для хранения всех вхождений входного массива
+
+    //
+    for (int i = 0; i < N; i++)      // Элементы из ввода сопоставленны с индексами
+        countArray[inputArray[i]]++;
+    //
+
+    //
+    for (int i = 1; i <= M; i++)            // Выичсиление суммы префикса по каждому индексу
+        countArray[i] += countArray[i - 1];
+    //
+
+    // Создания массива вывода
+    vector<int> outputArray(N);
+
+    for (int i = N - 1; i >= 0; i--) {
+        outputArray[countArray[inputArray[i]] - 1]
+            = inputArray[i];
+
+        countArray[inputArray[i]]--;
     }
+    return outputArray;
+    //
 }
 //
 
@@ -45,12 +57,11 @@ int main()
         cin >> arr[i];
     }
 
-    countingSort(arr);
-    cout << "Отсортированный массив: ";
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
+    // Вывод
+    vector<int> outputArray = countSort(arr);
+    for (int i = 0; i < arr.size(); i++)
+        cout << outputArray[i] << " ";
+    //
 
     return 0;
 
