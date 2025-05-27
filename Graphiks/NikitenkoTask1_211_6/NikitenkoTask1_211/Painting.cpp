753c076d31095a0bc418094cc3b0fd23d44b0f70
Harpie <silvia.lesnaia@gmail.com>
2025-05-27 22:27:28 +0400

    Начало 6 работы. Пока просто копия 4 работы

---
+++ Graphiks/NikitenkoTask1_211_6/NikitenkoTask1_211/Painting.cpp
@@ -0,0 +1,23 @@
+#include "Matrix.h"
+#include "Transform.h"
+#include <vector>
+#include "Figure.h"
+#include <fstream>
+#include <sstream>
+#include "Clip.h"
+#include "Painting.h"
+
+
+ using namespace System;
+ using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+ void Main(array<String^> ^ args) {
+	 Application::EnableVisualStyles();
+	 Application::SetCompatibleTextRenderingDefault(false);
+	 NikitenkoTask1211::Painting form;
+	 Application::Run(% form);
+	
+}
+
+
