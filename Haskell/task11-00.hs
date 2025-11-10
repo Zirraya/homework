55e64eadfb1461325b9cc0b34cdd9211b3c19c97
Harpie <silvia.lesnaia@gmail.com>
2025-11-10 12:19:49 +0400

    Немного занчения в первой лабе по хаскелю переделаны. Лабы по standard ML не совсем корректно оформлены. Надо исправть. Если нужно будет

--- Haskell/task11-00.hs
+++ Haskell/task11-00.hs
@@ -6,39 +6,39 @@ f11 x eps n
     y0 = 1.0
     y1 = x^2
     y2 = x^3 / 2
-    n' = min n 1000  -- Ограничиваем максимальную длину
+    n' = min n 50  -- Ограничение максимальной длинны
     
-    -- Более безопасная реализация без циклических ссылок
+    -- Реализация без цикал, оно не работает
     ys = y0 : y1 : y2 : [ nextValue i (ys !! (i-1)) (ys !! (i-2)) (ys !! (i-3)) | i <- [3..n'] ]
     
     nextValue i yn yn1 yn2 = (x * yn + yn2^2) / (fromIntegral i * yn1)
 
--- Упрощенная реализация zipWith4 (на всякий случай оставляем)
+-- Упрощенная реализация zipWith4 
 zipWith4 :: (a -> b -> c -> d -> e) -> [a] -> [b] -> [c] -> [d] -> [e]
 zipWith4 f (a:as) (b:bs) (c:cs) (d:ds) = f a b c d : zipWith4 f as bs cs ds
 zipWith4 _ _ _ _ _ = []
 
--- Упрощенные тестовые запуски
+-- Тестовые запуски
 main = do
   putStrLn "Тестирование функции f11"
   putStrLn $ "=" ++ replicate 40 '='
   
   -- Тест с меньшими значениями
-  let result1 = f11 0.3 0.001 50
+  let result1 = f11 2.3 0.001 50
   putStrLn "Тест 1: f11 0.3 0.001 50"
   putStrLn $ "Длина результата: " ++ show (length result1)
   putStrLn $ "10-й элемент: " ++ (if length result1 > 9 then show (result1 !! 9) else "не существует")
   putStrLn $ "50-й элемент: " ++ (if length result1 > 49 then show (result1 !! 49) else "не существует")
   putStrLn ""
   
-  let result2 = f11 0.5 0.001 50
+  let result2 = f11 1.5 0.001 50
   putStrLn "Тест 2: f11 0.5 0.001 50"
   putStrLn $ "Длина результата: " ++ show (length result2)
   putStrLn $ "10-й элемент: " ++ (if length result2 > 9 then show (result2 !! 9) else "не существует")
   putStrLn $ "50-й элемент: " ++ (if length result2 > 49 then show (result2 !! 49) else "не существует")
   putStrLn ""
   
-  let result3 = f11 1.7 0.001 50
+  let result3 = f11 1.8 0.001 50
   putStrLn "Тест 3: f11 1.7 0.001 50"
   putStrLn $ "Длина результата: " ++ show (length result3)
   putStrLn $ "10-й элемент: " ++ (if length result3 > 9 then show (result3 !! 9) else "не существует")
